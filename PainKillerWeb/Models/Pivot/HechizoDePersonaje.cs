2fb442742faae304628cee0e2471e3acd8f51100
*Facundo* <*facundoezequielmartinez@gmail.com*>
2022-12-10 21:10:41 -0300

    hechizo de personaje

---
+++ PainKillerWeb/Models/Pivot/HechizoDePersonaje.cs
@@ -0,0 +1,21 @@
+﻿using PainKillerWeb.Models.Main;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PainKillerWeb.Models.Pivot
+{
+    public class HechizoDePersonaje
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "´Personaje")]
+        public int personajeId { get; set; }
+        [Required]
+        [Display(Name = "Hechizo")]
+        public int HechizoId { get; set; }
+
+        public Personaje Personaje { get; set; }
+        public Hechizo Hechizo{ get; set; }
+    }
+}
